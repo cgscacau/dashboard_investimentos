95f368cbdef832ed2d1fbc9ca5acc56afb236807
cgscacau <129020973+cgscacau@users.noreply.github.com>
2025-10-19 20:23:46 -0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,21 @@
+"""
+Módulos de análise do Dashboard de Investimentos.
+
+Este pacote contém os módulos principais para análise de:
+- Ações individuais
+- Fundos de investimento
+- Comparação entre múltiplos ativos
+"""
+
+from . import analise_acoes
+from . import analise_fundos
+from . import comparacao
+
+__all__ = [
+    'analise_acoes',
+    'analise_fundos',
+    'comparacao'
+]
+
+__version__ = '2.0.0'
 
